eb5a58366c997b9fc5b275e7b7626d69621ba930
Mohammed-Hafeez-99 <upscsskzm805@gmail.com>
2025-03-05 12:42:56 +0530

    incomplete inheritance

---
+++ Employee.java
@@ -0,0 +1,17 @@
+public class Employee {
+
+    String name;
+    int age;
+    String phonenumber;
+    String address;
+    double salary;
+    public Employee(String name, int age,String phonenumber, String address,double salary)
+    {
+        this.name= name;
+        this.age= age;
+        this.phonenumber=phonenumber;
+        this.address=address;
+        this.salary=salary;
+        
+    }
+}
